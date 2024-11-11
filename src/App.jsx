7ea132f11b6f4aa6193b795a0b0680658ae4bc2e
Md-Naim-Howlader <mdnaimptk776@gmail.com>
2024-11-11 16:34:04 +0600

    added new project and components

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,29 @@
+import { useEffect, useState } from "react";
 import "./App.css";
+import BackToTop from "./Components/BackToTop";
 import DarkModeProvider from "./Context/DarkModeContext";
 import MainLayout from "./Layout/MainLayout";
+import Preloader from "./Components/Preloader";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+    return () => clearTimeout(timer);
+  }, []);
   return (
     <>
-      <DarkModeProvider>
-        <MainLayout />
-      </DarkModeProvider>
+      {loading ? (
+        <Preloader />
+      ) : (
+        <DarkModeProvider>
+          <MainLayout />
+          <BackToTop />
+        </DarkModeProvider>
+      )}
     </>
   );
 }
---
+++ src/Components/Preloader.jsx
@@ -0,0 +1,12 @@
+import "../Styles/preloader.css";
+// import logo from "../assets/images/logo/logo_2.png";
+const Preloader = () => {
+  return (
+    <div className="preloader">
+      <img src="/N_logo-1.png" alt="logo" />
+      <div className="loaderIcon"></div>
+    </div>
+  );
+};
+
+export default Preloader;
